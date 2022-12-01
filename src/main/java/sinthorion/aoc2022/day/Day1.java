e01b50a44cda61a733f3b111d34f241965383dab
Lukas Kuhn <l.kuhn@myopenfactory.com>
2022-12-01 21:33:39 +0100

    Day 1 complete

--- src/main/java/sinthorion/aoc2022/day/Day1.java
+++ src/main/java/sinthorion/aoc2022/day/Day1.java
@@ -1,16 +1,22 @@
 package sinthorion.aoc2022.day;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import sinthorion.aoc2022.system.DaySolver;
 
+@SuppressWarnings("unused")
 public class Day1 implements DaySolver {
 
   @Override
   public String solveA(String input) {
-    return null;
+    return String.valueOf(Arrays.stream(input.split("\\n\\n")).mapToInt(section -> section.lines()
+        .mapToInt(Integer::parseInt).sum()).max().orElseThrow());
   }
 
   @Override
   public String solveB(String input) {
-    return null;
+    return String.valueOf(Arrays.stream(input.split("\\n\\n")).mapToInt(section -> section.lines()
+        .mapToInt(Integer::parseInt).sum()).boxed().sorted(Comparator.reverseOrder()).mapToInt(i->i)
+        .limit(3).sum());
   }
 }
